cbf610fa1c5b0a1959809f85e91a9b6b711aa6b4
Michael <kontakt@ozum.pl>
2024-02-06 23:36:16 +0100

    feat: add content to mainpage

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,15 @@
 const Home = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Lorem ipsum
+                <br className="max-md:hidden" />
+                <span className="green_gradient text-center">Lorem ipsum dolor.</span>
+            </h1>
+            <p className="desc text-center">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dolorem est id molestiae numquam qui quia sint tempora.
+            </p>
+        </section>
     );
 };
 
