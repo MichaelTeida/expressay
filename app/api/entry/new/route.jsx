48d741c1ee13f623c5e36eb611c4f6697b1e97d2
Michael <kontakt@ozum.pl>
2024-02-09 22:44:18 +0100

    feat(entry/new/route.jsx): add functionality to create entry

--- app/api/entry/new/route.jsx
+++ app/api/entry/new/route.jsx
@@ -1,11 +1,17 @@
 import { connectToDB} from "@utils/database";
+import Entry from "@models/entry";
 
 export const POST = async (req) => {
-    const { usetId, entry, tag } = await req.json()
+    const { userId, entry, tag } = await req.json()
 
     try {
         await connectToDB
-    } catch (error) {
+        const newEntry = new Entry({creator: userId, entry, tag})
 
+        await newEntry.save()
+
+        return new Response(JSON.stringify(newEntry), {status: 201})
+    } catch (error) {
+        return new Response('Failed to create a new entry', { status: 500 })
     }
 }
