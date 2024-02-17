154043c8425913b6cdd753a10a049ac59f0c2dd1
Michael <kontakt@ozum.pl>
2024-02-17 23:35:28 +0100

    style(global.css): update styles for homepage text with tailwind

--- app/page.jsx
+++ app/page.jsx
@@ -2,15 +2,15 @@ import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Lorem ipsum
-        <br className="max-md:hidden" />
-        <span className="green_gradient text-center"> Lorem ipsum dolor.</span>
+    <section className="w-full flex-col">
+      <h1 className="head_text">
+        <span className="green_gradient text-center">
+          Express and share swiftly
+        </span>
       </h1>
-      <p className="desc text-center">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur,
-        dolorem est id molestiae numquam qui quia sint tempora.
+      <p className="desc">
+        Create, share, and explore entries from a diverse community of users.
+        Connect, inspire, and discover through the power of expression.
       </p>
 
       <Feed />
